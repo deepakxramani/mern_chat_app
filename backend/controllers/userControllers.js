356f530852d5676d65120180bbdb120e74642b7a
Deepak Ramani <deepakramani65@gmail.com>
2024-12-03 20:12:48 +0530

    added all chat endpoints

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -62,4 +62,20 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
-module.exports = { registerUser, authUser };
+// /api/user/?search=piyush
+
+const allUsers = asyncHandler(async (req, res) => {
+    const keyword = req.query.search ? {
+        $or: [
+            { name: {$regex: req.query.search, $options: "i"}},
+            { email: {$regex: req.query.search, $options: "i"}}
+        ]
+    } : {};
+
+    // console.log(keyword);
+
+    const users = await User.find(keyword).find({_id:{$ne:req.user._id}})   // search every user except the second find() method user
+    res.send(users)
+})
+
+module.exports = { registerUser, authUser, allUsers };
